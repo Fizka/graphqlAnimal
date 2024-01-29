a59a755388db896cbcc8ea8cb4805d749adfa1b2
kbo <krzysztof.bojes@wp.pl>
2024-01-29 23:37:10 +0100

    animal resolver, swagger

--- demo/src/main/java/com/ab/demo/resolver/AnimalResolver.java
+++ demo/src/main/java/com/ab/demo/resolver/AnimalResolver.java
@@ -1,4 +1,23 @@
 package com.ab.demo.resolver;
 
-public class AnimalResolver {
+import com.ab.demo.domain.Animal;
+import com.ab.demo.domain.Owner;
+import com.ab.demo.repository.OwnerRepository;
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalResolver implements GraphQLResolver<Animal> {
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    public AnimalResolver(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    public Owner getOwner(Animal animal) {
+        return ownerRepository.findById(animal.getOwner().getId())
+                .orElseThrow(null);
+    }
 }
